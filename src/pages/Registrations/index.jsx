0feaf3a0d1592668a11785f175d6ed506f4f56a6
rafaelcarvalho-git <rafaskyplay@gmail.com>
2023-07-06 00:55:12 -0300

    diferenciando tipos de usuarios

--- src/pages/Registrations/index.jsx
+++ src/pages/Registrations/index.jsx
@@ -8,13 +8,17 @@ import listCollaborators from 'services/collaborator/listCollaborators'
 
 const Registrations = () => {
   const [collaborators, setCollaborators] = useState([])
+  const [lawyers, setLawyers] = useState([])
+  const [offices, setOffices] = useState([])
   const [loading, setLoading] = useState(true)
 
   const fetchCollaborators = useCallback(() => {
     setLoading(true)
     listCollaborators()
       .then(({ data }) => {
-        setCollaborators(data.filter(u => u.email !== 'admin@gmail.com'))
+        setCollaborators(data.filter(u => u.passwordResetToken !== 'Lawyer' && u.passwordResetToken !== 'Office'))
+        setLawyers(data.filter(l => l.passwordResetToken === 'Lawyer'))
+        setOffices(data.filter(o => o.passwordResetToken === 'Office'))
         setLoading(false)
       })
       .catch(error => {
@@ -41,6 +45,8 @@ const Registrations = () => {
       ) : (
         <ListRegistrations
           collaborators={collaborators}
+          lawyers={lawyers}
+          offices={offices}
           successCallback={fetchCollaborators}
         />
       )}
