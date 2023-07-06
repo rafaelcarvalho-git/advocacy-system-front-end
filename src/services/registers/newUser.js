0760766ed94a1f1762080cd98732a56b023d1092
rafaelcarvalho-git <rafaskyplay@gmail.com>
2023-07-05 23:52:01 -0300

    removendo arquivos nao utilizados

--- src/services/registers/newUser.js
+++
@@ -1,7 +0,0 @@
-import collaboratorClient from '.'
-
-const newCollaborator = async userCreate => {
-  return await collaboratorClient.post('/newCollaborator', userCreate)
-}
-
-export default newCollaborator
