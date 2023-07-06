7003de75cd7201d947e670c55f2478b18c070b96
rafaelcarvalho-git <rafaskyplay@gmail.com>
2023-07-05 23:53:07 -0300

    movendo funcoes do modal para componentes filhos

--- src/components/Registrations/RegistrationsOptions/NewRegister/index.jsx
+++ src/components/Registrations/RegistrationsOptions/NewRegister/index.jsx
@@ -1,36 +1,16 @@
-import * as S from './styles'
-import { useState } from 'react'
 import NewRegisterUser from './User'
 import NewRegisterLawyer from './Lawyer'
 import NewRegisterOffice from './Office'
 
 
-const NewRegister = ({ open, setIsOpen, successCallback, userType, type }) => {
-    const [registerFunction, setRegisterFunction] = useState(null)
-
-    const handleOk = () => {
-        if (registerFunction) {
-            registerFunction()
-        }
-    }
-
-    const handleCancel = () => {
-        setIsOpen(false)
-    }
-
+const NewRegister = ({ open, setIsOpen, successCallback, userType }) => {
     return (
-        <S.RegisterModal
-            title={`Cadastrar Novo ${type}`}
-            open={open}
-            onOk={handleOk}
-            onCancel={handleCancel}
-            width={600}
-        >
-            {userType === 'users' && <NewRegisterUser successCallback={successCallback} setRegisterFunction={setRegisterFunction} />}
-            {userType === 'lawyers' && <NewRegisterLawyer successCallback={successCallback} setRegisterFunction={setRegisterFunction} />}
-            {userType === 'offices' && <NewRegisterOffice successCallback={successCallback} setRegisterFunction={setRegisterFunction} />}
+        <>
+            {userType === 'users' && <NewRegisterUser open={open} setIsOpen={setIsOpen} successCallback={successCallback} />}
+            {userType === 'lawyers' && <NewRegisterLawyer open={open} setIsOpen={setIsOpen} successCallback={successCallback} />}
+            {userType === 'offices' && <NewRegisterOffice open={open} setIsOpen={setIsOpen} successCallback={successCallback} />}
 
-        </S.RegisterModal>
+        </>
     )
 }
 
