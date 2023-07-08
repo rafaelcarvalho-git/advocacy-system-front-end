016463268c0177ba70787b158383862f89a88a88
rafaelcarvalho-git <rafaskyplay@gmail.com>
2023-07-08 01:20:20 -0300

    mudando foto perfil

--- src/components/Container/Navbar/UserProfile/index.jsx
+++ src/components/Container/Navbar/UserProfile/index.jsx
@@ -2,6 +2,7 @@ import * as S from './styles'
 import { Avatar, Tooltip } from '@mui/material'
 import LogoutIcon from '@mui/icons-material/Logout'
 import { getUserCachedData } from 'utils/UserAuthorization'
+import profile from 'assets/images/profile.jpg'
 
 
 const UserProfile = ({ setLogout }) => {
@@ -11,7 +12,7 @@ const UserProfile = ({ setLogout }) => {
     return (
         <S.ProfileOpt>
             <h4>{userName}</h4>
-            <Avatar alt='User' src='/static/images/avatar/2.jpg' />
+            <Avatar alt='User' src={profile} />
             <Tooltip title='Logout'>
                 <S.LogoutButton onClick={() => setLogout(true)}>
                     <LogoutIcon />
